3280259ee16159855ee256843b445647a69cc63d
nasirhaleemmalik565-debug <nasirhaleemmalik565@gmail.com>
2026-08-05 03:34:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Nursing by NHM Loaded Successfully");
+document.addEventListener("DOMContentLoaded", () => {
+
+console.log("Nursing by NHM Loaded");
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+card.addEventListener("click",()=>{
+card.style.transform="scale(.96)";
+setTimeout(()=>{
+card.style.transform="scale(1)";
+},150);
+});
+});
+
 });
