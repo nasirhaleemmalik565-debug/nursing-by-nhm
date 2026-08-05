412eedd1f8de0a45b060533e0020053cd5fb8b2f
Nursing by NHM <nasirhaleemmalik565@gmail.com>
2026-08-06 03:51:14 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
-const CACHE_NAME = "nhm-cache-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js"
-];
+const CACHE_NAME = "nhm-cache-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
