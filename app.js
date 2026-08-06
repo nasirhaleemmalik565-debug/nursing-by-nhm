27e86f4370e455637547e02f7ce49fc9dbc3fe45
Nursing by NHM <nasirhaleemmalik565@gmail.com>
2026-08-06 05:42:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-console.log("Nursing by NHM Loaded");
-
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card=>{
-card.addEventListener("click",()=>{
-card.style.transform="scale(.96)";
-setTimeout(()=>{
-card.style.transform="scale(1)";
-},150);
-});
-});
-
-});
-
-// ==========================
-// FAVORITES
-// ==========================
-
-document.querySelectorAll(".fav-btn").forEach(btn => {
-
-    btn.addEventListener("click", function(){
-
-        this.classList.toggle("active");
-
-        if(this.classList.contains("active")){
-            this.innerHTML = "❤️";
-        }else{
-            this.innerHTML = "♡";
-        }
-
-    });
-
-});
-
-// ==========================
-// FAVORITES SYSTEM
-// ==========================
-
 function toggleFav(btn){
-
-    btn.classList.toggle("active");
 
     const icon = btn.querySelector(".material-icons-outlined");
 
+    btn.classList.toggle("active");
+
     if(btn.classList.contains("active")){
-        icon.textContent = "favorite";
+
+        icon.textContent="favorite";
+
     }else{
-        icon.textContent = "favorite_border";
+
+        icon.textContent="favorite_border";
+
     }
 
 }
