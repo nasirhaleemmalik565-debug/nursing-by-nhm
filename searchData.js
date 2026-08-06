f812a08781eb99df7f81ccb9d8c28c5484021f46
Nursing by NHM <nasirhaleemmalik565@gmail.com>
2026-08-06 19:14:41 +0530

    Update searchData.js

--- searchData.js
+++ searchData.js
@@ -1,3 +1,29 @@
 const searchData = [
 
+
+{
+title:"Most common congenital heart disease",
+page:"mostcommon.html"
+},
+
+{
+title:"Most common cyanotic congenital heart disease",
+page:"mostcommon.html"
+},
+
+{
+title:"Most common acyanotic congenital heart disease",
+page:"mostcommon.html"
+},
+
+{
+title:"Most common artery involved in Myocardial Infarction",
+page:"mostcommon.html"
+},
+
+{
+title:"Most common organism causing Urinary Tract Infection",
+page:"mostcommon.html"
+}
+
 ];
